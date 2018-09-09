e1562923f9352702b640408a9d565749e72529f8
SKipoh <jack_burne@yahoo.co.uk>
2018-09-09 20:02:22 +0100

    Adding various functions for server handling,  message handling and broadcasting

--- Server.py
+++ Server.py
@@ -22,5 +22,41 @@ def accept_incoming_connections():
         client.send(bytes("Greetings from the cave!"+
                             "Now type your name and press enter!",
                             "utf8"))
-        address[client] = client_address
+        addresses[client] = client_address
         Thread(target=handle_client, args=(client,)).start()
+
+#Takes client socket as an argument
+def handle_client(client):
+    #Handles single client connection
+    name = client.recv(BUFSIZ).decode("utf8"))
+    welcome = 'Welcome %s! If you ever want to quit, type {quit} to exit' % name
+    client.send(bytes(welcome, "utf8"))
+    msg = "%s has joined the chat!" % name
+    broadcast(bytes(msg, "utf8"))
+    clients[client] = name
+    while True:
+        msg = client.recv(BUFSIZ)
+        if msg != bytes("{quit}", "utf8")):
+            broadcast(msg, name+": ")
+        else:
+            client.send(bytes("{quit}", "utf8"))
+            client.close()
+            del clients[client]
+            broadcast(bytes("%s has left the chat." % name, "utf8"))
+            break
+
+# prefix is the name for identification
+def broadcast(msg, prefix=""):
+    #Broadcasts messages to all the connected clients
+    for sock in clients:
+        sock.send(bytes(prefix, "utf8") +msg)
+
+if __name__ == "__main__":
+    #Listens for up to 5 maximum connections
+    SERVER.listen(5)
+    print("Waiting for connection...")
+    ACCEPT_THREAD = thread(target=accept_incoming_connections)
+    #Starts the infinite loop
+    ACCEPT_THREAD.start()
+    ACCEPT_THREAD.join
+    SERVER.close()
